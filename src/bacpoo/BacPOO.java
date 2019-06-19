e1c4a8da027081cb0c13168a2255f6cda0255117
Van <Van@DESKTOP-8JVUP0F>
2019-06-19 10:49:31 -0600

    XD

--- src/bacpoo/BacPOO.java
+++ src/bacpoo/BacPOO.java
@@ -16,6 +16,7 @@ public class BacPOO {
      */
     public static void main(String[] args) {
         System.out.println("Hola");
+        //ALV ESTA COSA NO DEJA SUBIR SI EL ADMIN MODIFICA A CADA RATO :V
     }
     
 }
