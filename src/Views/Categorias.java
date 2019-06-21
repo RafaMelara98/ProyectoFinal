9e79d1f5c8e868cc4c3edc2b9f80ad35d56c6328
Van <Van@DESKTOP-8JVUP0F>
2019-06-20 20:19:04 -0600

    Creando el Main

--- src/Views/Categorias.java
+++ src/Views/Categorias.java
@@ -149,7 +149,6 @@ public class Categorias extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Categorias().setVisible(true);
             }
         });
     }
